c29d31b4c6ee7005d3a4dfe15cb71922216c6d34
JayasinghePM <pasindu@climatesi.com>
2023-08-23 10:33:22 +0530

    add queue

---
+++ src/queues/Queues_1.java
@@ -0,0 +1,37 @@
+package queues;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+/*Queues - Data structure like Stacks
+ *(Like people queue, people join from the back leave from the front)
+ *FIFO - First In First Out
+ * Uses....
+     * printers
+     * Operating sys
+     * web servers(managing incoming requests)
+     * Live Support Systems
+ * Operations
+     * enqueue()
+     * dequeue()
+     * peek()
+     * isEmpty()
+     * isFull()
+ isFull()
+ */
+public class Queues_1 {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(11);
+        queue.add(12);
+        queue.add(13);
+        queue.remove();
+        System.out.println(queue);
+
+    }
+}
---
+++ src/queues/ReversingQueue.java
@@ -0,0 +1,32 @@
+package queues;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReversingQueue {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        reverse(queue);
+        System.out.println(queue);
+    }
+
+
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+        while (!stack.isEmpty()){
+           queue.add(stack.pop());
+        }
+    }
+
+
+
+
+}
