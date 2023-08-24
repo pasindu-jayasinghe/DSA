ac18e714aadf99371f2e645d2509c17c48b73f42
pasindu-jayasinghe <pmjayasinghe1025@gmail.com>
2023-08-24 11:04:39 +0530

    queue read.me update

--- src/queues/Queues_1.java
+++ src/queues/Queues_1.java
@@ -1,26 +1,8 @@
 package queues;
-
 import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.Queue;
 
-/*Queues - Data structure like Stacks
- *(Like people queue, people join from the back leave from the front)
- *FIFO - First In First Out
- * Uses....
-     * printers
-     * Operating sys
-     * web servers(managing incoming requests)
-     * Live Support Systems
- * Operations
-     * enqueue()
-     * dequeue()
-     * peek()
-     * isEmpty()
-     * isFull()
- isFull()
- */
+
 public class Queues_1 {
 
     public static void main(String[] args) {
