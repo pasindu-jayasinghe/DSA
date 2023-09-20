5dcef3a5e040b92762e257411b501c5d14edd257
pasindu-jayasinghe <pmjayasinghe1025@gmail.com>
2023-09-20 11:51:18 +0530

    queue using two stacks hackerrank

---
+++ src/challanges/java/hackerrank/datastructures/queues/QueueUsingTwoStacks.java
@@ -0,0 +1,48 @@
+package challanges.java.hackerrank.datastructures.queues;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class QueueUsingTwoStacks {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for(int i = 0; i < q; i++){
+            int type = sc.nextInt();
+            if(type == 1){
+                int x = sc.nextInt();
+                queue.enqueue(x);
+            }
+            else if(type == 2){
+                queue.dequeue();
+            }
+            else if(type == 3){
+                System.out.println(queue.peek());
+            }
+        }
+        sc.close();
+    }
+}
+class Queue<T> {
+    Stack<T> leftStack = new Stack<>();
+    Stack<T> rightStack = new Stack<>();
+    void enqueue(T item){
+        leftStack.push(item);
+    }
+    T dequeue(){
+        transferIfNeeded();
+        return rightStack.pop();
+    }
+    T peek(){
+        return rightStack.peek();
+    }
+    void transferIfNeeded(){
+        if(rightStack.empty()){
+            while(!leftStack.isEmpty()){
+                rightStack.push(leftStack.pop());
+            }
+        }
+    }
+}
